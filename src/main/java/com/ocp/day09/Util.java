93675088164672db418a5390b50cb8013e3fad85
boddy <boddy@DESKTOP-DLSUEFA>
2021-03-12 21:19:15 +0800

    Java OCPJP

---
+++ src/main/java/com/ocp/day09/Util.java
@@ -0,0 +1,13 @@
+package com.ocp.day09;
+
+// 工具類別
+public class Util {
+    // 兩數相加
+    public static int add(int x , int y){
+        if(x >= 50000){
+            return x + 2*y;
+        }
+        return x + y;
+    }
+    
+}
