79d4ea685fe8dc5ebc4da798217a0d45b4a5846f
boddy <boddy@DESKTOP-DLSUEFA>
2021-03-15 20:03:34 +0800

    Java OCPJP

---
+++ src/main/java/com/ocp/day10/ExamDemo.java
@@ -0,0 +1,36 @@
+package com.ocp.day10;
+
+
+class Bar{
+
+    Bar() {
+        System.out.println("A");
+    }
+    Bar(int x){
+        this();
+        System.out.println("B");
+    }
+    
+    
+}
+
+
+class Foo extends Bar{
+
+    Foo() {
+        this(10);   // 呼叫自己的建構子
+        System.out.println("C");
+    }
+    
+    Foo(int x){
+        super(20);  // 呼叫父類的建構子
+        System.out.println("D");
+    }
+}
+
+
+public class ExamDemo {
+    public static void main(String[] args) {
+        Foo foo = new Foo();
+    }
+}
--- src/main/java/com/ocp/day10/Son.java
+++ src/main/java/com/ocp/day10/Son.java
@@ -4,6 +4,7 @@ package com.ocp.day10;
 public class Son extends Father{
     // 因為 Son 繼承 Father 所以 Father 物件會先被建立，然後 Son 物件才被建立
     public Son() {
+        // super();  因為這裡有一個隱含的 super()
         System.out.println("兒子被建立");
     }
     
@@ -11,7 +12,7 @@ public class Son extends Father{
     @Override
     public void hobby() {
         //super.hobby();
-        System.out.println("LoL");
+        System.out.println("LoL");  // Son 自己的興趣
     }
 
            
