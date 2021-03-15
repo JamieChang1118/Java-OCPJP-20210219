45de71c9b80e820e1c1b74fc9d7b261087886419
boddy <boddy@DESKTOP-DLSUEFA>
2021-03-15 19:21:13 +0800

    Java OCPJP

--- src/main/java/com/ocp/day10/VarageDemo.java
+++ src/main/java/com/ocp/day10/VarageDemo.java
@@ -5,21 +5,36 @@ import java.util.stream.IntStream;
 
 public class VarageDemo {
     public static void main(String[] args) {
+        // 經過 Compiler Sugar 以下都會是一樣的(時間)
+        // A
+        if(true){
+            System.out.println("Hello Word");
+        }
+        // B
+        System.out.println("Hello Word");
+        
+        System.out.println(10+20+30);
+        System.out.println(60);
+        
         System.out.println(add(10, 20));
         System.out.println(add(10, 20, 30));
         int [] values ={10,20,30,40,50};
         System.out.println(add(values));
+        System.out.println(add(10, 20, 30, 40));
+        System.out.println(add());
+        
     }
     
-    public static int add(int x , int y){
-        return x + y;
-    }
+    // 超載(Overloading)
+//    public static int add(int x , int y){
+//        return x + y;
+//    }
+//    
+//    public static int add(int x , int y , int z){
+//        return x + y + z;
+//    }
     
-    public static int add(int x , int y , int z){
-        return x + y + z;
-    }
-    
-    public static int add(int[] values){
+    public static int add(int... values){
         return IntStream.of(values).sum();
     }
 }
