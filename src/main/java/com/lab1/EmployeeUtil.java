bfd95e96058ae4dd31271b2d961bdfd6964cffa5
boddy <boddy@DESKTOP-DLSUEFA>
2021-03-19 20:51:00 +0800

    Java OCPJP

--- src/main/java/com/lab1/EmployeeUtil.java
+++ src/main/java/com/lab1/EmployeeUtil.java
@@ -7,15 +7,19 @@ import java.util.stream.Stream;
 public class EmployeeUtil {
     // 取得所有員工的薪資
     public int getAllSalary(Employee... employees){
-        // Java 7
-//        int sum = 0;
-//        for(Employee e : employees){
-//            sum += e.getSalary();
-//        }
-//        return sum;
-        // Java 8
+    // Java 8
     //return Stream.of(employees).mapToInt(e -> e.getSalary()).sum();
-    return Stream.of(employees).mapToInt(Employee::getSalary).sum();
-        
+    return Stream.of(employees).mapToInt(Employee::getSalary).sum();   
     }
+    
+    // 取得所有經理的薪資
+    public int getAllSalary(Manager... managers){
+    return Stream.of(managers).mapToInt(Manager::getSalary).sum();   
+    }
+    
+    // 取得所有董事的薪資
+    public int getAllSalary(Director... directors){
+    return Stream.of(directors).mapToInt(Director::getSalary).sum();   
+    }
+    
 }
