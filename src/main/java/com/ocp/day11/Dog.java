34597f42a41cfbde4da79929ab06bcfd731ce4ce
boddy <boddy@DESKTOP-DLSUEFA>
2021-03-22 19:48:11 +0800

    Java OCPJP

---
+++ src/main/java/com/ocp/day11/Dog.java
@@ -0,0 +1,15 @@
+package com.ocp.day11;
+
+public class Dog extends AnimalImpl {
+
+    @Override
+    public void sound() {
+        System.out.println("旺旺");
+    }
+    
+//    @Override
+//    public void sleep() {
+//        System.out.println("Zzz");
+//    }
+    
+}
