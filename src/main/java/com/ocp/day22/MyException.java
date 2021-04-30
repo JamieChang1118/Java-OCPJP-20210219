5c43c9fdf578b5cbe33430e609d12ec5bf75a293
boddy <boddy@DESKTOP-DLSUEFA>
2021-04-30 21:39:04 +0800

    Java OCPJP

--- src/main/java/com/ocp/day22/MyException.java
+++ src/main/java/com/ocp/day22/MyException.java
@@ -3,7 +3,7 @@ package com.ocp.day22;
 public class MyException {
     public static void main(String[] args) {
         try {
-            checkFood("蛋");
+            checkFood("炸雞");
         } catch (BadFoodException e) {
             System.out.println(e.getMessage());
         } finally {
@@ -12,9 +12,13 @@ public class MyException {
     }
     
     public static void checkFood(String food)throws BadFoodException{
-        if(food == "可樂" || food == "薯條" || food == "炸雞"){
-            BadFoodException e = new BadFoodException(food);
-            throw e;
+        String[] badFoods = {"可樂","薯條","炸雞"};     // 垃圾食物
+        for(String bad : badFoods){
+            if(food.equals(bad)){
+                BadFoodException e = new BadFoodException(food);
+                throw e;
+            }
         }
+        System.out.println(food + " 這不是垃圾食物");
     }
 }
